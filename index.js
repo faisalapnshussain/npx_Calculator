410cccfdd95a7867269b05002e6e5d4911aff25a
Faisal Hussain <faisalapnshussain@gmail.com>
2024-06-19 10:19:00 +0500

    Uploading first NPX calculator

---
+++ index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env Node
+import inquirer from "inquirer";
+const result = await inquirer.prompt([
+    { message: "Enter your first number", type: "number", name: "firstnumber" },
+    { message: "select an operator to perform", name: "operator", type: "list", choices: ["+", "-", "*", "/", "**"] },
+    { message: "enter your second number", type: "number", name: "secondnumber" }
+]);
+//Conditional Statement
+if (result.operator === "+") {
+    console.log("The the result of addition is =", result.firstnumber + result.secondnumber);
+}
+else if (result.operator === "-") {
+    console.log("The the result of substraction is =", result.firstnumber - result.secondnumber);
+}
+else if (result.operator === "*") {
+    console.log("The the result of multiplication is =", result.firstnumber * result.secondnumber);
+}
+else if (result.operator === "/") {
+    console.log("The the result of division is =", result.firstnumber / result.secondnumber);
+}
+else if (result.operator === "**") {
+    console.log("The the result of exponention is =", result.firstnumber ** result.secondnumber);
+}
